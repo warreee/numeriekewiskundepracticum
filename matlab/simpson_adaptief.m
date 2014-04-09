7a1849be420a1080ec2f3495cb2081b6b38e5775
Ward Schodts <wardschodts@hotmail.com>
2014-04-09 16:20:09 +0200

    1.2.c

--- matlab/simpson_adaptief.m
+++ matlab/simpson_adaptief.m
@@ -1,10 +1,24 @@
 function [I] = simpson_adaptief( f,a,b,e )
 I1 = simpson(f,a,b,2);
 I2 = simpson(f,a,b,4);
-if abs(I1 - I2) < e
-    I = I2;
-else 
-    I = simpson_adaptief(f,a,(a+b)/2,e) + simpson_adaptief(f,(a+b)/2,b,e);
-end
+switcher = true;
+    if isfinite(I1) && isfinite(I2)
+         if abs(I1 - I2) < e
+            I = I2;
+    
+         else 
+         I = simpson_adaptief(f,a,(a+b)/2,e) + simpson_adaptief(f,(a+b)/2,b,e);
+         end
+    elseif switcher == true;
+        switcher = false;    
+        a = a + eps;
+        b = b -eps;
+        I = simpson_adaptief(f,a,(a+b)/2,e) + simpson_adaptief(f,(a+b)/2,b,e);
+    elseif switcher == false;
+        switcher = true;
+        a = a - eps;
+        b = b + eps;
+        I = simpson_adaptief(f,a,(a+b)/2,e) + simpson_adaptief(f,(a+b)/2,b,e);
+    end
 end
 
